3691c693747c58a0d62db6a346238d670ca9acbf
Johann Woelper <woelper@gmail.com>
2023-07-07 19:13:13 +0000

    init

--- src/app.rs
+++ src/app.rs
@@ -1,42 +1,36 @@
+use std::path::Path;
+use anyhow::Result;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct MicronApp {
     // Example stuff:
     label: String,
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+  
 }
 
-impl Default for TemplateApp {
+impl Default for MicronApp {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
-            value: 2.7,
         }
     }
 }
 
-impl TemplateApp {
+impl MicronApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for MicronApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -47,12 +41,7 @@ impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self { label, value } = self;
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
@@ -60,57 +49,42 @@ impl eframe::App for TemplateApp {
                     if ui.button("Quit").clicked() {
                         _frame.close();
                     }
+                    if ui.button("Open").clicked() {
+                        rfd::
+                        ui.ctx().clos
+                    }
                 });
             });
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
+        
+           
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+          
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
+      
     }
+}
+
+fn read_file(path: &Path) -> Result<Vec<u8>>{
+    use positioned_io::{RandomAccessFile, ReadAt};
+
+    // open a file (note: binding does not need to be mut)
+    let raf = RandomAccessFile::open(path)?;
+
+    // read up to 512 bytes
+    let mut buf = [0; 512];
+    let bytes_read = raf.read_at(2048, &mut buf)?;
+    Ok(buf.to_vec())
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct FileProperties {
+    cursor: u64,
+    buffer: Vec<u8>,
 }
