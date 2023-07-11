1eed359dd81451414bd5f372a80e4c538d51623a
Johann Woelper <woelper@gmail.com>
2023-07-11 12:32:00 +0200

    add test sample generation

---
+++ create_test_files.sh
@@ -0,0 +1,3 @@
+yes this is a 100 kB test file | head -c 100KB > test100kB.txt
+yes this is 100MB test file | head -c 100MB > test100MB.txt
+yes this is 1GB test file | head -c 1000MB > test1000MB.txt
