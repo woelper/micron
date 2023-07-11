89548ea371cef4ed309abdf2dd0513069cc1cb4a
Johann Woelper <woelper@gmail.com>
2023-07-11 23:00:50 +0200

    update test file script

--- create_test_files.sh
+++ create_test_files.sh
@@ -1,4 +1,4 @@
-for size in 1 10 100 1000;
+for size in 1 2 4 8 10 100 1000;
 do
     yes this is a $size MB test file | head -c ${size}MB > tmptest${size}MB.txt
     nl tmptest${size}MB.txt > test${size}MB.txt
