3c7c264217f50b511fd3f6b43d65377b7209c1d2
Johann Woelper <woelper@gmail.com>
2023-07-11 14:01:59 +0200

    test script to generate text with newlines

--- create_test_files.sh
+++ create_test_files.sh
@@ -1,3 +1,7 @@
-yes this is a 100 kB test file | head -c 100KB > test100kB.txt
-yes this is 100MB test file | head -c 100MB > test100MB.txt
-yes this is 1GB test file | head -c 1000MB > test1000MB.txt
+for size in 1 10 100 1000;
+do
+    yes this is a $size MB test file | head -c ${size}MB > tmptest${size}MB.txt
+    nl tmptest${size}MB.txt > test${size}MB.txt
+    rm tmptest${size}MB.txt
+    echo made a $size MB file
+done
