4a883f032d1b63fffd11e422b8d99357b5eae853
Johann Woelper <woelper@gmail.com>
2023-07-11 08:33:55 +0200

    highlighting

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use egui::{TextEdit, Vec2};
+use egui::{Color32, TextEdit, Vec2};
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
@@ -9,6 +9,7 @@ use std::{
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct MicronApp {
+    #[serde(skip)]
     open_files: HashMap<PathBuf, OpenedFile>,
     active_file: Option<PathBuf>,
 }
@@ -25,27 +26,34 @@ impl Default for MicronApp {
 impl MicronApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-
-
         let mut fonts = egui::FontDefinitions::default();
 
         // Install my own font (maybe supporting non-latin characters):
-        fonts.font_data.insert("mono".to_owned(),
-           egui::FontData::from_static(include_bytes!("../assets/FiraCode-Regular.ttf"))); // .ttf and .otf supported
+        fonts.font_data.insert(
+            "mono".to_owned(),
+            egui::FontData::from_static(include_bytes!("../assets/FiraCode-Regular.ttf")),
+        ); // .ttf and .otf supported
 
-        fonts.font_data.insert("sans".to_owned(),
-           egui::FontData::from_static(include_bytes!("../assets/FiraCode-Regular.ttf"))); // .ttf and .otf supported
+        fonts.font_data.insert(
+            "sans".to_owned(),
+            egui::FontData::from_static(include_bytes!("../assets/FiraCode-Regular.ttf")),
+        ); // .ttf and .otf supported
 
         // Put my font first (highest priority):
-        fonts.families.get_mut(&egui::FontFamily::Proportional).unwrap()
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Proportional)
+            .unwrap()
             .insert(0, "sans".to_owned());
 
         // Put my font as last fallback for monospace:
-        fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap()
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Monospace)
+            .unwrap()
             .insert(0, "mono".to_owned());
 
         cc.egui_ctx.set_fonts(fonts);
-
 
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
@@ -114,18 +122,44 @@ impl eframe::App for MicronApp {
                 egui::ScrollArea::vertical()
                     .auto_shrink([false, true])
                     .show(ui, |ui| {
-                        if ui
-                            .add(
-                                TextEdit::multiline(&mut text)
-                                    .frame(false)
-                                    .margin(Vec2::new(2., 2.))
-                                    .desired_width(f32::INFINITY)
-                                    .code_editor(),
-                            )
-                            .changed()
-                        {
-                            opened_file.buffer = text.into_bytes();
-                        }
+                        let mut theme =
+                            crate::syntax_highlighting::CodeTheme::from_memory(ui.ctx());
+                        ui.collapsing("Theme", |ui| {
+                            ui.group(|ui| {
+                                theme.ui(ui);
+                                theme.clone().store_in_memory(ui.ctx());
+                            });
+                        });
+
+                        let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
+                            let mut layout_job = crate::syntax_highlighting::highlight(
+                                ui.ctx(),
+                                &theme,
+                                string,
+                                "rs",
+                            );
+                            layout_job.wrap.max_width = wrap_width;
+                            ui.fonts(|f| f.layout_job(layout_job))
+                        };
+
+                        egui::ScrollArea::vertical().show(ui, |ui| {
+                            if ui
+                                .add(
+                                    egui::TextEdit::multiline(&mut text)
+                                        .font(egui::TextStyle::Monospace) // for cursor height
+                                        .code_editor()
+                                        .desired_rows(10)
+                                        .lock_focus(true)
+                                        .desired_width(f32::INFINITY)
+                                        .frame(false)
+                                        .margin(Vec2::new(2., 2.))
+                                        .layouter(&mut layouter),
+                                )
+                                .changed()
+                            {
+                                opened_file.buffer = text.into_bytes();
+                            }
+                        });
                     });
             }
         });
